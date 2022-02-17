5625d7e2478f7d207c9169ba361fe848e52020d5
Cristhian <cristhian.smaia@gmail.com>
2022-02-16 21:35:34 -0300

    importacoes e ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AuthGuard} from "./shared/guards/auth-guard";
+import {HomeComponent} from "./pages/home/home.component";
+import {CavaloComponent} from "./pages/cavalo/cavalo.component";
+import {PainelAdministrativoComponent} from "./pages/painel-administrativo/painel-administrativo.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {SignupComponent} from "./pages/signup/signup.component";
 
 const routes: Routes = [
-  { path: 'login' },
-  { path: 'home', canActivate: [AuthGuard] },
+  { path: 'registro', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cavalo/:id', component: CavaloComponent },
+  { path: 'painel-administrativo', component: PainelAdministrativoComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,44 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AuthGuard} from "./shared/guards/auth-guard";
+import { HomeComponent } from './pages/home/home.component';
+import { HorseCardComponent } from './components/horse-card/horse-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CavaloComponent } from './pages/cavalo/cavalo.component';
+import { PainelAdministrativoComponent } from './pages/painel-administrativo/painel-administrativo.component';
+import {HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
+import { SignupComponent } from './pages/signup/signup.component';
+import { CavalosCadastradosComponent } from './pages/painel-administrativo/components/cavalos-cadastrados/cavalos-cadastrados.component';
+import { CadastrarCavaloComponent } from './pages/painel-administrativo/components/cadastrar-cavalo/cadastrar-cavalo.component';
+import {httpInterceptorProviders} from "./shared/interceptors/httpInterceptorProvider";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HorseCardComponent,
+    NavbarComponent,
+    CavaloComponent,
+    PainelAdministrativoComponent,
+    LoginComponent,
+    SignupComponent,
+    CavalosCadastradosComponent,
+    CadastrarCavaloComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        FormsModule,
+        MatIconModule
+    ],
   providers: [
-    AuthGuard
+    AuthGuard,
+    httpInterceptorProviders,
   ],
   bootstrap: [AppComponent]
 })
